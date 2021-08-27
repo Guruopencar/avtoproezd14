952e563074c3fa33be67f7931515990d43ca4c15
srv171887 <srv171887@c15m.hoster.ru>
2021-08-27 17:24:34 +0300

    first commit

---
+++ catalog/language/ru-ru/common/footer.php
@@ -0,0 +1,3 @@
+<?php
+// Text
+$_['text_information']  = 'Информация';
---
+++ catalog/language/ru-ru/product/search.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']     = 'Поиск';
+$_['heading_tag']       = 'По тегу - ';
+
+// Text
+$_['text_search']       = 'Результаты поиска';
+$_['text_keyword']      = 'Ключевые слова';
+$_['text_category']     = 'Все категории';
+$_['text_sub_category'] = 'Поиск в подкатегориях';
+$_['text_empty']        = 'Нет товаров, которые соответствуют критериям поиска.';
+$_['text_quantity']     = 'Количество:';
+$_['text_manufacturer'] = 'Производитель:';
+$_['text_model']        = 'Модель:';
+$_['text_points']       = 'Бонусные баллы:';
+$_['text_price']        = 'Цена:';
+$_['text_tax']          = 'Без НДС:';
+$_['text_reviews']      = 'Всего отзывов: %s';
+$_['text_compare']      = 'Сравнение товаров (%s)';
+
+// Entry
+$_['entry_search']      = 'Поиск:';
+$_['entry_description'] = 'Искать в описании товаров';
+
